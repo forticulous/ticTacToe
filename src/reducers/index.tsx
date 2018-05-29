f6482924a24e5143a42966a575c2bf6dc4e2f587
forticulous <forticulous@gmail.com>
2018-05-29 16:16:25 -0700

    Better logic for reducer

--- src/reducers/index.tsx
+++ src/reducers/index.tsx
@@ -30,29 +30,23 @@ export type BoardSquare = 'cross' | 'circle' | 'blank'
 
 export type BoardState = Array<Array<BoardSquare>>
 
-const setColumn = (
-  row: Array<BoardSquare>,
-  column: number,
-  newValue: BoardSquare,
-): Array<BoardSquare> => {
-  return Array.of(
-    column === 0 ? newValue : row[0],
-    column === 1 ? newValue : row[1],
-    column === 2 ? newValue : row[2],
-  )
-}
-
 const setSquare = (
   board: BoardState,
   row: number,
   column: number,
   newValue: BoardSquare,
 ): BoardState => {
-  return Array.of(
-    row === 0 ? setColumn(board[0], column, newValue) : Array.from(board[0]),
-    row === 1 ? setColumn(board[1], column, newValue) : Array.from(board[1]),
-    row === 2 ? setColumn(board[2], column, newValue) : Array.from(board[2]),
-  )
+  return board.map((currentRow, rowIndex) => {
+    if (row === rowIndex) {
+      return currentRow.map((square, colIndex) => {
+        if (column === colIndex) {
+          return newValue
+        }
+        return square
+      })
+    }
+    return currentRow
+  })
 }
 
 export const crossConcernReducer = (
