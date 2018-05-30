ceb2d1d138d9942b1a8ffb57152122cad8c4b0c0
forticulous <forticulous@gmail.com>
2018-05-30 16:17:20 -0700

    Use loops to for the board layout

--- src/components/board.tsx
+++ src/components/board.tsx
@@ -16,48 +16,18 @@ export type Props = InertProps & ActionProps
 const Board = ({ board, clickBoard }: Props) => {
   return (
     <div className={styles.mainContent}>
-      <div className={styles.row}>
-        <BoardSquareComp
-          type={board[0][0]}
-          clickSquare={clickBoard.bind(null, 0, 0)}
-        />
-        <BoardSquareComp
-          type={board[0][1]}
-          clickSquare={clickBoard.bind(null, 0, 1)}
-        />
-        <BoardSquareComp
-          type={board[0][2]}
-          clickSquare={clickBoard.bind(null, 0, 2)}
-        />
-      </div>
-      <div className={styles.row}>
-        <BoardSquareComp
-          type={board[1][0]}
-          clickSquare={clickBoard.bind(null, 1, 0)}
-        />
-        <BoardSquareComp
-          type={board[1][1]}
-          clickSquare={clickBoard.bind(null, 1, 1)}
-        />
-        <BoardSquareComp
-          type={board[1][2]}
-          clickSquare={clickBoard.bind(null, 1, 2)}
-        />
-      </div>
-      <div className={styles.row}>
-        <BoardSquareComp
-          type={board[2][0]}
-          clickSquare={clickBoard.bind(null, 2, 0)}
-        />
-        <BoardSquareComp
-          type={board[2][1]}
-          clickSquare={clickBoard.bind(null, 2, 1)}
-        />
-        <BoardSquareComp
-          type={board[2][2]}
-          clickSquare={clickBoard.bind(null, 2, 2)}
-        />
-      </div>
+      {[0, 1, 2].map(row => {
+        return (
+          <div className={styles.row}>
+            {[0, 1, 2].map(col =>
+              <BoardSquareComp
+                type={board[row][col]}
+                clickSquare={clickBoard.bind(null, row, col)}
+              />
+            )}
+          </div>
+        )
+      })}
     </div>
   )
 }
